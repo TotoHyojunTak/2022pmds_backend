e558105dc39d607ea5df620d6873a9e44971d481
thj0309 <thj0309@hotmail.com>
2022-10-02 17:41:49 +0900

    파일 수정

--- src/main/java/com/backend/controller/HelloController.java
+++ src/main/java/com/backend/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
+@Api(value="HelloController Swagger", tags = "HelloController")
 public class HelloController {
 
-    @GetMapping("/helloworld")
+    @RequestMapping(value="/helloworld", method= RequestMethod.GET)
     public String helloworld(){
         return "backend - helloworld";
     }
