53ffca1ed07259712c931737fbec19310f68ca02
thj0309 <thj0309@hotmail.com>
2022-10-04 15:38:35 +0900

    modified

--- src/main/java/com/backend/controller/HealthController.java
+++ src/main/java/com/backend/controller/HealthController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/hello")
-@Api(value="HelloController Swagger", tags = "HelloController")
-public class HelloController {
+@RequestMapping("/health")
+@Api(value="HealthController Swagger", tags = "HealthController")
+public class HealthController {
 
-    @RequestMapping(value="/helloworld", method= RequestMethod.GET)
-    public String helloworld(){
-        return "backend - helloworld";
+    // 생략
+    @GetMapping("/info")
+    public String info(@Value("${server.port}") String port) {
+        return "서비스의 기본 동작 Port: {" + port + "}";
     }
-
 }
