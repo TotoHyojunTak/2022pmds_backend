5d403c479fa1a8a7544a7a51984489a5e415a5b7
thj0309 <thj0309@cj.net>
2022-10-10 12:07:25 +0900

    jdk 변경

--- src/main/java/com/backend/controller/HelloController.java
+++ src/main/java/com/backend/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.backend.controller;
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
