922cbd7a9f84f38dedc56d6f26b98c4bf8394fdc
thj0309 <thj0309@cj.net>
2022-10-21 11:54:21 +0900

    modi

--- src/main/java/com/backend/BackendApplication.java
+++ src/main/java/com/backend/BackendApplication.java
@@ -1,7 +1,6 @@
 package com.backend;
 
 import feign.Logger;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -17,7 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        enc();
+        //enc();
 
         SpringApplication.run(BackendApplication.class, args);
     }
@@ -28,58 +27,58 @@ public class BackendApplication {
         return Logger.Level.FULL;
     }
 
-    private static void enc(){
-        String url = "jdbc:log4jdbc:mariadb://146.56.114.57:3306/user";
-        String username = "user";
-        String password = "user1234!@#$";
-
-        String encryptUrl = jasyptEncrypt(url);
-        String encryptUsername = jasyptEncrypt(username);
-        String encryptPassword = jasyptEncrypt(password);
-
-        System.out.println("user encryptUrl : " + encryptUrl);
-        System.out.println("user encryptUsername : " + encryptUsername);
-        System.out.println("user encryptPassword : " + encryptPassword);
-
-        url = "jdbc:log4jdbc:mariadb://146.56.114.57:3306/order";
-        username = "order";
-        password = "order1234!@#$";
-
-        encryptUrl = jasyptEncrypt(url);
-        encryptUsername = jasyptEncrypt(username);
-        encryptPassword = jasyptEncrypt(password);
-
-        System.out.println("order encryptUrl : " + encryptUrl);
-        System.out.println("order encryptUsername : " + encryptUsername);
-        System.out.println("order encryptPassword : " + encryptPassword);
-
-        url = "jdbc:log4jdbc:mariadb://146.56.114.57:3306/catalog";
-        username = "catalog";
-        password = "catalog1234!@#$";
-
-        encryptUrl = jasyptEncrypt(url);
-        encryptUsername = jasyptEncrypt(username);
-        encryptPassword = jasyptEncrypt(password);
-
-        System.out.println("catalog encryptUrl : " + encryptUrl);
-        System.out.println("catalog encryptUsername : " + encryptUsername);
-        System.out.println("catalog encryptPassword : " + encryptPassword);
-    }
-
-     private static String jasyptEncrypt(String input) {
-        String key = "A421080gsm";
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setAlgorithm("PBEWithMD5AndDES");
-        encryptor.setPassword(key);
-        return encryptor.encrypt(input);
-    }
-
-    private static String jasyptDecryt(String input){
-        String key = "A421080gsm";
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setAlgorithm("PBEWithMD5AndDES");
-        encryptor.setPassword(key);
-        return encryptor.decrypt(input);
-    }
+//    private static void enc(){
+//        String url = "jdbc:log4jdbc:mariadb://146.56.114.57:3306/user";
+//        String username = "user";
+//        String password = "user1234!@#$";
+//
+//        String encryptUrl = jasyptEncrypt(url);
+//        String encryptUsername = jasyptEncrypt(username);
+//        String encryptPassword = jasyptEncrypt(password);
+//
+//        System.out.println("user encryptUrl : " + encryptUrl);
+//        System.out.println("user encryptUsername : " + encryptUsername);
+//        System.out.println("user encryptPassword : " + encryptPassword);
+//
+//        url = "jdbc:log4jdbc:mariadb://146.56.114.57:3306/order";
+//        username = "order";
+//        password = "order1234!@#$";
+//
+//        encryptUrl = jasyptEncrypt(url);
+//        encryptUsername = jasyptEncrypt(username);
+//        encryptPassword = jasyptEncrypt(password);
+//
+//        System.out.println("order encryptUrl : " + encryptUrl);
+//        System.out.println("order encryptUsername : " + encryptUsername);
+//        System.out.println("order encryptPassword : " + encryptPassword);
+//
+//        url = "jdbc:log4jdbc:mariadb://146.56.114.57:3306/catalog";
+//        username = "catalog";
+//        password = "catalog1234!@#$";
+//
+//        encryptUrl = jasyptEncrypt(url);
+//        encryptUsername = jasyptEncrypt(username);
+//        encryptPassword = jasyptEncrypt(password);
+//
+//        System.out.println("catalog encryptUrl : " + encryptUrl);
+//        System.out.println("catalog encryptUsername : " + encryptUsername);
+//        System.out.println("catalog encryptPassword : " + encryptPassword);
+//    }
+//
+//     private static String jasyptEncrypt(String input) {
+//        String key = "A421080gsm";
+//        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+//        encryptor.setAlgorithm("PBEWithMD5AndDES");
+//        encryptor.setPassword(key);
+//        return encryptor.encrypt(input);
+//    }
+//
+//    private static String jasyptDecryt(String input){
+//        String key = "A421080gsm";
+//        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+//        encryptor.setAlgorithm("PBEWithMD5AndDES");
+//        encryptor.setPassword(key);
+//        return encryptor.decrypt(input);
+//    }
 
 }
