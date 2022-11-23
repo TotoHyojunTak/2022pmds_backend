630718edf80be911da4c5d4e30885deb7dd3a7fe
thj0309 <thj0309@cj.net>
2022-11-23 15:51:02 +0900

    test

--- src/main/java/com/backend/feign/OrderServiceClient.java
+++ src/main/java/com/backend/feign/OrderServiceClient.java
@@ -1,13 +1,14 @@
 package com.backend.feign;
 
 import com.backend.data.dto.response.OrderDTO;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="2022pmds-backend-order")
+@FeignClient(name="2022pmds-backend-order", fallback = FallbackFactory.Default.class)
 public interface OrderServiceClient {
     @GetMapping("/order/{userId}/orders")
     List<OrderDTO> getOrders(@PathVariable String userId);
